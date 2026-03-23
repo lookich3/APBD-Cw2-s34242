5a497795e16651c4cdf84dddef08114c418f20e4
Oleksii Nyskoroda <s34242@pjwtsk.edu.pl>
2026-03-23 18:13:00 +0100

    Dodalem model wypozyczenia i generator identyfikatorow

---
+++ Models/Rental.cs
@@ -0,0 +1,33 @@
+﻿namespace APBDcw2.Models;
+
+public class Rental
+{
+    public int Id { get; set; }
+    public User User { get; set; }
+    public Equipment Equipment { get; set; }
+    public DateTime RentDate { get; set; }
+    public DateTime DueDate { get; set; }
+    public DateTime? ReturnDate { get; set; }
+    public decimal Penalty { get; set; }
+
+    public bool IsReturned => ReturnDate.HasValue;
+    public bool IsActive => !ReturnDate.HasValue;
+
+    public Rental(int id, User user, Equipment equipment, DateTime rentDate, DateTime dueDate)
+    {
+        Id = id;
+        User = user;
+        Equipment = equipment;
+        RentDate = rentDate;
+        DueDate = dueDate;
+        ReturnDate = null;
+        Penalty = 0;
+    }
+
+    public override string ToString()
+    {
+        string returnInfo = ReturnDate.HasValue ? ReturnDate.Value.ToShortDateString() : "nie zwrócono";
+
+        return $"Id wypożyczenia: {Id}, Użytkownik: {User.FirstName} {User.LastName}, Sprzęt: {Equipment.Name}, Data wypożyczenia: {RentDate.ToShortDateString()}, Termin zwrotu: {DueDate.ToShortDateString()}, Data zwrotu: {returnInfo}, Kara: {Penalty}";
+    }
+}
---
+++ Services/IdGenerator.cs
@@ -0,0 +1,11 @@
+﻿namespace APBDcw2.Services;
+
+public class IdGenerator
+{
+    private int _currentId = 1;
+
+    public int GetNextId()
+    {
+        return _currentId++;
+    }
+}
