2df4908a69f33f6df378390c066d9f3876d510a4
Oleksii Nyskoroda <s34242@pjwtsk.edu.pl>
2026-03-24 19:51:04 +0100

    Dodalem serwisy do zarzadzania sprzetem i uzytkownikami

---
+++ Services/EquipmentService.cs
@@ -0,0 +1,48 @@
+﻿using APBDcw2.Models;
+
+namespace APBDcw2.Services;
+
+public class EquipmentService
+{
+    private readonly List<Equipment> _equipmentList = new();
+
+    public void AddEquipment(Equipment equipment)
+    {
+        _equipmentList.Add(equipment);
+    }
+
+    public List<Equipment> GetAllEquipment()
+    {
+        return _equipmentList;
+    }
+
+    public List<Equipment> GetAvailableEquipment()
+    {
+        return _equipmentList
+            .Where(equipment => equipment.Status == EquipmentStatus.Available)
+            .ToList();
+    }
+
+    public Equipment? GetById(int id)
+    {
+        return _equipmentList.FirstOrDefault(equipment => equipment.Id == id);
+    }
+
+    public bool MarkAsUnavailable(int equipmentId)
+    {
+        var equipment = GetById(equipmentId);
+
+        if (equipment == null)
+        {
+            return false;
+        }
+
+        if (equipment.Status == EquipmentStatus.Rented)
+        {
+            return false;
+        }
+
+        equipment.Status = EquipmentStatus.Unavailable;
+        return true;
+    }
+}
---
+++ Services/UserService.cs
@@ -0,0 +1,23 @@
+﻿using APBDcw2.Models;
+
+namespace APBDcw2.Services;
+
+public class UserService
+{
+    private readonly List<User> _users = new();
+
+    public void AddUser(User user)
+    {
+        _users.Add(user);
+    }
+
+    public List<User> GetAllUsers()
+    {
+        return _users;
+    }
+
+    public User? GetById(int id)
+    {
+        return _users.FirstOrDefault(user => user.Id == id);
+    }
+}
